d15e1ad01e3aa493c5bad4dd001edbdcb7f11459
Patrick Schleizer <adrelanos@riseup.net>
2017-02-24 22:46:51 +0000

    add tor_consensus_valid-after.py / tor_consensus_valid-until.py

---
+++ usr/lib/anon-shared-helper-scripts/tor_consensus_valid-until.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3 -u
+
+## This file is part of Whonix.
+## Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
+## See the file COPYING for copying conditions.
+
+import sys
+from stem.connection import connect
+
+controller = connect()
+
+if not controller:
+    sys.exit(255)
+
+output = controller.get_info("consensus/valid-until")
+
+print(format(output))
+
+controller.close()
+
+sys.exit(0)
