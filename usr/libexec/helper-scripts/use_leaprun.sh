a6644656b66ebe2c0bbfcbad5bf3d0e172142304
Patrick Schleizer <adrelanos@whonix.org>
2025-02-06 16:29:47 -0500

    improve leaprun_useable_test

--- usr/libexec/helper-scripts/use_leaprun.sh
+++ usr/libexec/helper-scripts/use_leaprun.sh
@@ -3,16 +3,42 @@
 ## Copyright (C) 2025 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-use_leaprun='no'
+leaprun_useable_test() {
+   use_leaprun='no'
 
-if ! leaprun_exe="$(command -v leaprun)"; then
-   echo "$0: WARNING: leaprun executable cannot be found, cannot use privleap."
-elif ! [ -f "/run/privleapd/pid" ]; then
-   echo "$0: WARNING: Cannot check if privleapd is not running, cannot use privleap."
-elif ! [ -d "/proc/$(cat /run/privleapd/pid)" ]; then
-   echo "$0: WARNING: privleapd is not running, cannot use privleap."
-elif ! [ -e "/run/privleapd/comm/$(id -nu)" ]; then
-   echo "$0: WARNING: Cannot communicate with privleapd, cannot use privleap."
-else
+   if ! leaprun_exe="$(command -v leaprun)"; then
+      echo "$0: WARNING: leaprun executable cannot be found. Cannot use privleap."
+      return 0
+   fi
+
+   if ! [ -f "/run/privleapd/pid" ]; then
+      echo "$0: WARNING: Cannot check if privleapd is not running. Cannot use privleap."
+      return 0
+   fi
+
+   local privleap_pid
+   if ! privleap_pid="$(cat /run/privleapd/pid)"; then
+      echo "$0: WARNING: Failed to execute 'cat /run/privleapd/pid'. Cannot use privleap."
+      return 0
+   fi
+
+   if ! [ -d "/proc/${privleap_pid}" ]; then
+      echo "$0: WARNING: privleapd is not running. Cannot use privleap."
+      return 0
+   fi
+
+   local my_user_id
+   if ! my_user_id="$(id --name --user)"; then
+      echo "$0: WARNING: Failed to execute 'id --name --user'. Cannot use privleap."
+      return 0
+   fi
+
+   if ! [ -e "/run/privleapd/comm/${my_user_id}" ]; then
+      echo "$0: WARNING: Cannot communicate with privleapd. Cannot use privleap."
+      return 0
+   fi
+
    use_leaprun='yes'
-fi
+}
+
+leaprun_useable_test
