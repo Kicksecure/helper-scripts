788a2424485528e07c187dbd6b0ff698709ecf1b
Patrick Schleizer <adrelanos@whonix.org>
2025-02-13 22:18:41 -0500

    print warnings to stderr

--- usr/libexec/helper-scripts/use_leaprun.sh
+++ usr/libexec/helper-scripts/use_leaprun.sh
@@ -21,34 +21,34 @@ leaprun_useable_test() {
 #    fi
 
    if ! leaprun_exe="$(command -v leaprun)"; then
-      echo "$0: WARNING: leaprun executable cannot be found. Cannot use privleap."
+      echo "$0: WARNING: leaprun executable cannot be found. Cannot use privleap." >&2
       return 0
    fi
 
    if ! [ -f "/run/privleapd/pid" ]; then
-      echo "$0: WARNING: Cannot check if privleapd is not running. Cannot use privleap."
+      echo "$0: WARNING: Cannot check if privleapd is not running. Cannot use privleap." >&2
       return 0
    fi
 
    local privleap_pid
    if ! privleap_pid="$(cat /run/privleapd/pid)"; then
-      echo "$0: WARNING: Failed to execute 'cat /run/privleapd/pid'. Cannot use privleap."
+      echo "$0: WARNING: Failed to execute 'cat /run/privleapd/pid'. Cannot use privleap." >&2
       return 0
    fi
 
    if ! [ -d "/proc/${privleap_pid}" ]; then
-      echo "$0: WARNING: privleapd is not running. Folder '/proc/${privleap_pid}' does not exist. Cannot use privleap."
+      echo "$0: WARNING: privleapd is not running. Folder '/proc/${privleap_pid}' does not exist. Cannot use privleap." >&2
       return 0
    fi
 
    local my_user_id
    if ! my_user_id="$(id --name --user)"; then
-      echo "$0: WARNING: Failed to execute 'id --name --user'. Cannot use privleap."
+      echo "$0: WARNING: Failed to execute 'id --name --user'. Cannot use privleap." >&2
       return 0
    fi
 
    if ! [ -e "/run/privleapd/comm/${my_user_id}" ]; then
-      echo "$0: WARNING: Cannot communicate with privleapd. File '/run/privleapd/comm/${my_user_id}' does not exist. Cannot use privleap."
+      echo "$0: WARNING: Cannot communicate with privleapd. File '/run/privleapd/comm/${my_user_id}' does not exist. Cannot use privleap." >&2
       return 0
    fi
 
