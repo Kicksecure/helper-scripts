3a9760349212aaeb02089891f6ad873e8a13dfcf
Patrick Schleizer <adrelanos@whonix.org>
2025-02-13 22:17:53 -0500

    comment

--- usr/libexec/helper-scripts/use_leaprun.sh
+++ usr/libexec/helper-scripts/use_leaprun.sh
@@ -6,6 +6,20 @@
 leaprun_useable_test() {
    use_leaprun='no'
 
+   ## privleap / leaprun is supposed to work fine even if called as root.
+   ## Due to preference for failing early, hard and noisy, not giving special
+   ## treatment for account 'root'.
+#    local id_user
+#    if ! id_user="$(id --user)" ; then
+#       echo "$0: WARNING: Cannot run 'id --user'. Cannot use privleap." >&2
+#       return 0
+#    fi
+#
+#    if [ "$id_user" = "0" ] ; then
+#       true "$0: INFO: Already using account root / id 0. No need to use privleap."
+#       return 0
+#    fi
+
    if ! leaprun_exe="$(command -v leaprun)"; then
       echo "$0: WARNING: leaprun executable cannot be found. Cannot use privleap."
       return 0
