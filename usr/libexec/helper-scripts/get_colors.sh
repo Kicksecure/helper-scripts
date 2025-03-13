b88cca5a8abf8df527f433c370aa62a5d4f681d1
Patrick Schleizer <adrelanos@whonix.org>
2025-03-13 06:54:35 -0400

    check additional conditions to disable colors in unsupported terminals

--- usr/libexec/helper-scripts/get_colors.sh
+++ usr/libexec/helper-scripts/get_colors.sh
@@ -5,19 +5,20 @@
 get_colors(){
   get_colors_sourced=1
   ## Disable colors if some environment variables are present.
-  if test -n "${NO_COLOR:-}" || test -n "${ANSI_COLORS_DISABLED:-}"; then
-    nocolor=""
-    bold=""
-    nobold=""
-    underline=""
-    nounderline=""
-    red=""
-    green=""
-    yellow=""
-    blue=""
-    magenta=""
-    cyan=""
-    return 0
+  if test -n "${NO_COLOR:-}" || test -n "${ANSI_COLORS_DISABLED:-}" || \
+    test -z "${TERM:-}" || test "${TERM:-}" = "dumb" || test "${TERM:-}" = "unknown"; then
+      nocolor=""
+      bold=""
+      nobold=""
+      underline=""
+      nounderline=""
+      red=""
+      green=""
+      yellow=""
+      blue=""
+      magenta=""
+      cyan=""
+      return 0
   fi
 
   printf -v nocolor     '%b' "\033[0m"
