2fa5e6a72f148da1b3476615c26e7839dea0c4de
Aaron Rainbolt <arraybolt3@ubuntu.com>
2025-05-04 22:54:18 -0500

    Make live-mode.sh more robust and able to detect a semi-persistent state

--- usr/libexec/helper-scripts/live-mode.sh
+++ usr/libexec/helper-scripts/live-mode.sh
@@ -7,33 +7,51 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
   set -o errexit
   set -o nounset
   set -o errtrace
-  set -o pipefail
+#  set -o pipefail
 fi
 
-if [ -z "${proc_cmdline_output+x}" ]; then
-  proc_cmdline_output="$(cat -- /proc/cmdline)"
+was_pipefail_enabled='false'
+if [ -o pipefail ]; then
+  was_pipefail_enabled='true'
+  set +o pipefail
+fi
+
+if [ -z "${mount_command_output+x}" ]; then
+  mount_command_output="$(mount)"
 fi
 
 ## Detect if the system was booted in live mode
 ## Check for 'rd.live.image' first, because both, ISO and grub-live come with 'boot=live' kernel parameter.
-if printf "%s" "${proc_cmdline_output}" | grep --quiet --fixed-strings -e 'root=live' -e 'rd.live.image'; then
+if printf "%s" "${mount_command_output}" 2>/dev/null | grep --quiet 'LiveOS_rootfs on / type overlay'; then
   live_status_detected_live_mode_environment_pretty="ISO Live"
   live_status_detected_live_mode_environment_machine="iso-live"
   live_status_word_pretty="ISO"
   live_status_detected="true"
   live_status_maybe_iso_live_message="<br/><u>This message can be safely ignored if only using this ISO to install to the hard drive.</u><br/>"
-elif printf "%s" "${proc_cmdline_output}" | grep --quiet --fixed-strings -e 'boot=live' -e 'rootovl' -e 'rd.live.overlay.overlayfs=1' ; then
-  live_status_detected_live_mode_environment_pretty="grub-live"
-  live_status_detected_live_mode_environment_machine="grub-live"
-  live_status_word_pretty="Live"
-  live_status_detected="true"
-  live_status_maybe_iso_live_message=""
+elif printf "%s" "${mount_command_output}" 2>/dev/null | grep --quiet 'overlay on / type overlay'; then
+  if printf "%s" "${mount_command_output}" 2>/dev/null | grep --quiet '/dev/.* on /home ' ; then
+    live_status_detected_live_mode_environment_pretty="grub-live-semi-persistent"
+    live_status_detected_live_mode_environment_machine="grub-live-semi-persistent"
+    live_status_word_pretty="Semi-persistent"
+    live_status_detected="true"
+    live_status_maybe_iso_live_message=""
+  else
+    live_status_detected_live_mode_environment_pretty="grub-live"
+    live_status_detected_live_mode_environment_machine="grub-live"
+    live_status_word_pretty="Live"
+    live_status_detected="true"
+    live_status_maybe_iso_live_message=""
+  fi
 else
   live_status_detected_live_mode_environment_pretty="false"
   live_status_detected_live_mode_environment_machine="false"
   live_status_word_pretty="persistent"
   live_status_detected="false"
   live_status_maybe_iso_live_message=""
+fi
+
+if [ "$was_pipefail_enabled" = 'true' ]; then
+  set -o pipefail
 fi
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
