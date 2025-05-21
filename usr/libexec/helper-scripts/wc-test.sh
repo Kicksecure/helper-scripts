685831b8e733a8f3f1b12ec4c7f14504b8be7f1e
Patrick Schleizer <adrelanos@whonix.org>
2025-05-21 11:47:28 -0400

    shellcheck

--- usr/libexec/helper-scripts/wc-test.sh
+++ usr/libexec/helper-scripts/wc-test.sh
@@ -3,8 +3,8 @@
 ## Copyright (C) 2012 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-if ! printf '%s\n' | wc -l >/dev/null ; then
-  printf "\
+if ! printf '%s\n' "" | wc -l >/dev/null ; then
+  printf '%s\n' "\
 $0: ERROR: command 'wc' test failed! Do not ignore this!
 
 'wc' can core dump. Example:
