93152b9ab74e76719b8916bea3f75318f0261446
Patrick Schleizer <adrelanos@whonix.org>
2025-05-21 07:21:35 -0400

    add `usr/libexec/helper-scripts/wc-test.sh`

---
+++ usr/libexec/helper-scripts/wc-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Copyright (C) 2012 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
+## See the file COPYING for copying conditions.
+
+if ! printf '%s\n' | wc -l >/dev/null ; then
+  printf "\
+$0: ERROR: command 'wc' test failed! Do not ignore this!
+
+'wc' can core dump. Example:
+zsh: illegal hardware instruction (core dumped) wc -l
+https://github.com/rspamd/rspamd/issues/5137" >&2
+  exit 1
+fi
