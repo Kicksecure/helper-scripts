08ea7fbce0baf576304754ac9e1267014a81f42f
Aaron Rainbolt <arraybolt3@ubuntu.com>
2025-06-06 20:23:08 -0500

    Improve get_writable_fs_lists.sh, add tests for get_writable_fs_lists.sh and live-mode.sh

--- usr/libexec/helper-scripts/get_writable_fs_lists.sh
+++ usr/libexec/helper-scripts/get_writable_fs_lists.sh
@@ -17,6 +17,8 @@ if [ -z "${proc_mount_contents+x}" ]; then
   proc_mount_contents="$(cat /proc/self/mounts)"
 fi
 
+[ -z "${sysfs_prefix}" ] && sysfs_prefix=""
+
 while read -r line; do
   IFS=' ' read -r -a line_parts <<< "${line}"
   if [ "${#line_parts[@]}" != '6' ]; then
@@ -26,29 +28,31 @@ while read -r line; do
   mount_point="${line_parts[1]}"
   fs_type="${line_parts[2]}"
   opt_str="${line_parts[3]}"
-  if [[ "${src_device}" =~ ^/dev/ ]] \
-    || [[ "${fs_type}" =~ ^(nfs|vboxsf|virtiofs|9pfs) ]]; then
-    is_physical_device='false'
-    if [[ "${src_device}" =~ ^/dev/ ]] \
-      && [ "$(printf '%s' "${src_device}" | sed 's/[^\/]//g' | wc -c)" = 2 ];
-      then is_physical_device='true'
-      src_device="${src_device##*/}"
-      if [[ "${src_device}" =~ ^(sd|vd|hd|xvd) ]]; then
+  is_physical_device='false'
+  is_network_drive='false'
+
+  if [[ "${src_device}" =~ ^/dev/ ]]; then
+    is_physical_device='true'
+    if [ "$(printf '%s' "${src_device}" | sed 's/[^\/]//g' | wc -c)" = 2 ];
+      then src_device="${src_device##*/}"
+      if [[ "${src_device}" =~ ^(sd|vd|hd|xvd) ]] \
+        && [[ "${src_device}" =~ (.*)([0-9]+) ]]; then
         ## cut off any numbers from the end, we don't want them
-        if [[ "${src_device}" =~ (.*)([0-9]+) ]]; then
-          src_device="${BASH_REMATCH[1]}"
-        fi
-      else
+        src_device="${BASH_REMATCH[1]}"
+      elif [[ "${src_device}" =~ (.*[0-9]+)(.+) ]]; then
         ## Assume we're dealing with a device type where the disk ID is like
         ## nbd0 and the partition ID is like nbd0p1 (this is the case for NBD
         ## devices, NVME drives, SD cards, some loop devices, etc.). In this
         ## instance we want to trim off the `p1` part.
-        if [[ "${src_device}" =~ (.*[0-9]+)(.+) ]]; then
-          src_device="${BASH_REMATCH[1]}"
-        fi
+        src_device="${BASH_REMATCH[1]}"
       fi
     fi
+  elif [[ "${fs_type}" =~ ^(nfs|vboxsf|virtiofs|9pfs) ]]; then
+    is_network_drive='true'
+  fi
 
+  if [ "${is_physical_device}" = 'true' ] \
+    || [ "${is_network_drive}" = 'true' ]; then
     IFS=',' read -r -a opt_parts <<< "${opt_str}"
     rw_found='false'
     for opt_part in "${opt_parts[@]}"; do
@@ -57,6 +61,7 @@ while read -r line; do
         break
       fi
     done
+
     if [ "${rw_found}" = 'true' ]; then
       if ! [[ "${mount_point}" =~ ^/media/ ]] \
         && ! [[ "${mount_point}" =~ ^/mnt/ ]] \
@@ -64,7 +69,10 @@ while read -r line; do
         && ! [ "${mount_point}" = '/mnt' ]; then
         danger_writable_fs_list+=( "${mount_point}" )
       elif [ "${is_physical_device}" = 'true' ] \
-        && [ "$(cat "/sys/class/block/${src_device}/removable")" = '0' ]; then
+        && [ -f "${sysfs_prefix}/sys/class/block/${src_device}/removable" ] \
+        && [ "$(
+          cat "${sysfs_prefix}/sys/class/block/${src_device}/removable"
+        )" = '0' ]; then
         danger_writable_fs_list+=( "${mount_point}" )
       else
         safe_writable_fs_list+=( "${mount_point}" )
