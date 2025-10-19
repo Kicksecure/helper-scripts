88f5d5e3de6599c9d4583c16c8964aae3e42a2e0
Patrick Schleizer <adrelanos@whonix.org>
2025-10-19 18:15:33 -0400

    Revert "lockfile.sh: custom error message version"
    This reverts commit e09cb422750489e6a8f2ec57504b22075e3d52bc.

--- usr/libexec/helper-scripts/lockfile.sh
+++ usr/libexec/helper-scripts/lockfile.sh
@@ -7,18 +7,12 @@
 ## Not supposed to be executed (except during development).
 ## Lock file mechanism to prevent duplicate script instances across users
 
-true "${BASH_SOURCE[0]} - $$ - $SHLVL: START"
+true "${BASH_SOURCE[0]}: START"
 
-true "${BASH_SOURCE[0]} - $$ - $SHLVL: INFO: FLOCKER: ${FLOCKER-}"
+true "${BASH_SOURCE[0]}: INFO: FLOCKER: ${FLOCKER-}"
 
 if [ "${FLOCKER-}" != "$0" ]; then
-  exit_code=0
-  env FLOCKER="$0" flock --verbose --conflict-exit-code 255 --exclusive --nonblock "$0" "$0" "$@" || exit_code="$?"
-  if [ "$exit_code" = 255 ]; then
-    true "${BASH_SOURCE[0]} - $$ - $SHLVL: ERROR: Already running."
-  fi
-  true "${BASH_SOURCE[0]} - $$ - $SHLVL: INFO: Exit with code: $exit_code"
-  exit "$exit_code"
+  exec env FLOCKER="$0" flock --verbose --exclusive --nonblock "$0" "$0" "$@"
 fi
 
-true "${BASH_SOURCE[0]} - $$ - $SHLVL: END"
+true "${BASH_SOURCE[0]}: END"
