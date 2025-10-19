feaea0c4cd856eb5d5c44a68cea7a9541048efb8
Patrick Schleizer <adrelanos@whonix.org>
2025-10-19 17:49:32 -0400

    fix

--- usr/libexec/helper-scripts/lockfile.sh
+++ usr/libexec/helper-scripts/lockfile.sh
@@ -7,65 +7,12 @@
 ## Not supposed to be executed (except during development).
 ## Lock file mechanism to prevent duplicate script instances across users
 
-my_id="$(id --user)"
-SCRIPT_NAME="$(basename -- "$0")"
-LOCKDIR="/run/user/${my_id}/lockfile"
-PIDFILE="${LOCKDIR}/${SCRIPT_NAME}.pid"
-LOCKFILE="${LOCKDIR}/${SCRIPT_NAME}.lock"
+true "${BASH_SOURCE[0]}: START"
 
-cleanup_lockfile() {
-  safe-rm -f -- "$PIDFILE"
-  ## Bad idea?
-  #safe-rm -f -- "$LOCKFILE"
-}
+true "${BASH_SOURCE[0]}: INFO: FLOCKER: ${FLOCKER-}"
 
-command -v safe-rm >/dev/null
-command -v stcat >/dev/null
-command -v overwrite >/dev/null
-
-if ! mkdir --parents -- "$LOCKDIR"; then
-  printf '%s\n' "$0: ERROR: Failed to create lock directory '$LOCKDIR'. Check permissions."
-  exit 1
+if [ "${FLOCKER-}" != "$0" ]; then
+  exec env FLOCKER="$0" flock --verbose --exclusive --nonblock "$0" "$0" "$@"
 fi
 
-if [ -d "$LOCKDIR" ]; then
-  if [[ ! -r "$LOCKDIR" || ! -w "$LOCKDIR" || ! -x "$LOCKDIR" ]]; then
-    printf '%s\n' "$0: ERROR: No rwx access to lock directory '$LOCKDIR'. Check permissions."
-    exit 1
-  fi
-fi
-
-if [ -e "$PIDFILE" ]; then
-  if ! [ -r "$PIDFILE" ] || ! [ -w "$PIDFILE" ]; then
-    printf '%s\n' "$0: ERROR: Cannot read/write to lock file '$PIDFILE'. Check permissions."
-    exit 1
-  fi
-
-  LOCK_PID=$(stcat "$PIDFILE" 2>/dev/null)
-  if [ -z "$LOCK_PID" ]; then
-    printf '%s\n' "$0: WARNING: Lock file exists but is empty. Removing stale lock file."
-    safe-rm -f -- "$PIDFILE"
-  elif ! [[ "$LOCK_PID" =~ ^[0-9]+$ ]]; then
-    printf '%s\n' "$0: WARNING: Lock file contains invalid PID. Removing stale lock file."
-    safe-rm -f -- "$PIDFILE"
-  elif kill -0 -- "$LOCK_PID" 2>/dev/null; then
-    printf '%s\n' "$0: ERROR: Another instance of '${SCRIPT_NAME}' is already running with PID '$LOCK_PID'."
-    exit 1
-  else
-    printf '%s\n' "$0: WARNING: Lock file contains PID '$LOCK_PID', but no process is running. Removing stale lock file."
-    safe-rm -f -- "$PIDFILE"
-  fi
-fi
-
-trap cleanup_lockfile EXIT
-
-if ! flock --nonblock --exclusive "$LOCKFILE" overwrite "$PIDFILE" "$$" >/dev/null ; then
-  trap '' EXIT
-  exit 1
-fi
-
-## Debugging.
-# if ! flock --verbose --exclusive --nonblock "$LOCKFILE" sleep 10 ; then
-#   trap '' EXIT
-#   exit 1
-# fi
+true "${BASH_SOURCE[0]}: END"
