49b7d2535a79e3213c7e80c88e033108d2a5bf98
Patrick Schleizer <adrelanos@whonix.org>
2025-10-23 01:33:32 -0400

    refactoring

--- usr/libexec/helper-scripts/boot-session-detection.sh
+++ usr/libexec/helper-scripts/boot-session-detection.sh
@@ -8,14 +8,14 @@ boot_session_detection() {
 
   kernel_cmdline="$(cat -- /proc/cmdline)"
 
-  if ! [[ "${kernel_cmdline}" =~ 'boot-role=sysmaint' ]]; then
-    true "INFO: USER Session detected."
-    true "INFO: (kernel parameter 'boot-role=sysmaint' is not present, ok.)"
-    boot_session="user_session"
-  else
+  if [[ "${kernel_cmdline}" =~ 'boot-role=sysmaint' ]]; then
     true "INFO: SYSMAINT Session detected."
     true "INFO: (kernel parameter 'boot-role=sysmaint' present, ok.)"
     boot_session="sysmaint_session"
+  else
+    true "INFO: USER Session detected."
+    true "INFO: (kernel parameter 'boot-role=sysmaint' is not present, ok.)"
+    boot_session="user_session"
   fi
 }
 
