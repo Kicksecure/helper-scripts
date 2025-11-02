057e40e433a7e7e62ddcfe363e18c61f54124dc1
Patrick Schleizer <adrelanos@whonix.org>
2025-11-02 04:08:25 -0500

    comments

--- usr/share/helper-scripts/policy-rc.d
+++ usr/share/helper-scripts/policy-rc.d
@@ -3,8 +3,11 @@
 ## Copyright (C) 2012 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-## Prevents daemons from starting while using 'apt-get'.
+## Can prevent daemons from starting while using 'apt-get' or 'apt-get-noninteractive'.
 ## Takes care of chroot mount from getting locked.
+
+## 'apt-get-noninteractive' sets environment variable 'POLICYRCD'.
+## Package 'policyrcd-script-zg2' supports environment variable 'POLICYRCD'.
 
 ## Therefore, for example, prevents connecting to the public Tor network while
 ## building the images. This is interesting for (obfuscated) bridge users and
