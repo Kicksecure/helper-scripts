b85c89d6b8d571d6ed9a0b7aae124a9ee3d42c64
Patrick Schleizer <adrelanos@whonix.org>
2025-11-02 04:04:43 -0500

    comments

--- usr/share/helper-scripts/policy-rc.d
+++ usr/share/helper-scripts/policy-rc.d
@@ -3,15 +3,13 @@
 ## Copyright (C) 2012 - 2025 ENCRYPTED SUPPORT LLC <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
-## Prevents daemons from starting while using apt-get.
+## Prevents daemons from starting while using 'apt-get'.
 ## Takes care of chroot mount from getting locked.
 
 ## Therefore for example prevents connecting to the public Tor network while
 ## building the images. This is interesting for (obfuscated) bridge users and
 ## also prevents sensitive data from the build machine, such as the Tor
-## consensus /var/lib/tor leaking into the image.
-
-## This file gets deleted by the build script at the end.
+## consensus files in '/var/lib/tor' folder leaking into the image.
 
 ## Thanks to
 ## http://lifeonubuntu.com/how-to-prevent-server-daemons-from-starting-during-apt-get-install/
