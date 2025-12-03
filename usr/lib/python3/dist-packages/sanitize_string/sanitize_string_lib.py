0d9cad74b2e1a8736334c5e685e47bfd1572b9f7
Aaron Rainbolt <arraybolt3@ubuntu.com>
2025-12-02 19:19:46 -0600

    sanitize_string: Add another escape code strip stage (bug found by ChatGPT Codex)

--- usr/lib/python3/dist-packages/sanitize_string/sanitize_string_lib.py
+++ usr/lib/python3/dist-packages/sanitize_string/sanitize_string_lib.py
@@ -19,6 +19,24 @@ def sanitize_string(untrusted_string: str) -> str:
     Sanitizer function.
     """
 
+    ## It isn't necessarily safe to strip ANSI codes first, then markup, since
+    ## markup may contain HTML entities that are translated to ESC characters.
+    ## CPython's HTML parser is smart enough to discard such entities, but
+    ## that could break in the future in theory.
+    ##
+    ## Similarly, it isn't safe to strip markup first, then ANSI codes, as
+    ## there may be a bug (now or in the future) in CPython's HTML parser that
+    ## would fail to strip an HTML tag with an embedded ESC character. That
+    ## tag could then be activated when the ESCs are translated to underscores
+    ## by stdisplay. Thus, we should either strip markup, strip escapes, then
+    ## strip markup again, or strip escapes, strip markup, then strip escapes
+    ## again.
+    ##
+    ## In benchmarking, stdisplay is anywhere between three and ten times
+    ## faster than strip_markup, thus we use "strip escapes, strip markup,
+    ## then strip escapes again."
+
     step_one_sanitized_string: str = stdisplay(untrusted_string, sgr=-1)
-    final_sanitized_string: str = strip_markup(step_one_sanitized_string)
+    step_two_sanitized_string: str = strip_markup(step_one_sanitized_string)
+    final_sanitized_string: str = stdisplay(step_two_sanitized_string, sgr=-1)
     return final_sanitized_string
--- usr/lib/python3/dist-packages/sanitize_string/tests/sanitize_string.py
+++ usr/lib/python3/dist-packages/sanitize_string/tests/sanitize_string.py
@@ -129,6 +129,7 @@ Won't blow up world, because it's commented :) \x1b[8mor not!\x1b[0m
 </script></head><body>
 <p>There really isn't bold text below, I promise!</p>
 <<b>b>Not bold!<</b>/b>
+<p>&#27;[8mThis text might become invisible.&#27;[0m</p>
 </body></html>
 """,
                 """\
@@ -138,6 +139,7 @@ _[8mor not!_[0m
 
 There really isn't bold text below, I promise!
 _b_Not bold!_/b_
+[8mThis text might become invisible.[0m
 
 """,
             ),
